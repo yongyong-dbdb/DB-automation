957c02fcc24c626fbe6551e83da78176d43b605e
yongyong-dbdb <c4ic79@kyonggi.ac.kr>
2026-07-08 16:33:59 +0900

    Update 01_instance_health.sql

--- PostgreSQL/postgres-daily-check/sql/01_instance_health.sql
+++ PostgreSQL/postgres-daily-check/sql/01_instance_health.sql
@@ -1,14 +1,14 @@
 \echo '## 01. Instance Health / Restart Check'
 SELECT
     current_database() AS database_name,
-    inet_server_addr() AS server_addr,
-    inet_server_port() AS server_port,
     pg_postmaster_start_time() AS postmaster_start_time,
-    now() - pg_postmaster_start_time() AS uptime,
+    CASE
+        WHEN pg_postmaster_start_time() > now() - interval '1 day'
+        THEN chr(27) || '[31m' || (now() - pg_postmaster_start_time())::text || chr(27) || '[0m'
+        ELSE (now() - pg_postmaster_start_time())::text
+    END AS uptime,
     CASE
         WHEN pg_postmaster_start_time() > now() - interval '1 day'
         THEN 'RESTARTED_WITHIN_24H'
         ELSE 'NO_RESTART_WITHIN_24H'
-    END AS restart_status,
-    version() AS postgres_version;
-
+    END AS restart_status;
